2911a521587a14c6b64039a28bd89939162408f6
Gurpreet Singh <gurpreetsingh.kainth@mail.click-labs.com>
2017-03-06 18:41:12 +0530

    change localhost

--- hapi.js
+++ hapi.js
@@ -5,7 +5,7 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({ 
-    host: 'localhost', 
+    host: '192.168.43.34', 
     port: 8000 
 });
 
